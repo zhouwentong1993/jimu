75c3ee9469691afab0329d045a059027d7a42367
zhouwentong <zhouwentong@hualala.com>
2023-03-28 15:18:25 +0800

    update

--- core/src/main/java/com/wentong/jimu/flow/dispatcher/MemoryFlowDispatcher.java
+++ core/src/main/java/com/wentong/jimu/flow/dispatcher/MemoryFlowDispatcher.java
@@ -5,6 +5,7 @@ import com.wentong.jimu.flow.Flow;
 import com.wentong.jimu.flow.task.FlowTask;
 import com.wentong.jimu.flow.task.Task;
 import com.wentong.jimu.flow.task.TaskResult;
+import com.wentong.jimu.flow.task.TaskStatusEnum;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
@@ -27,6 +28,8 @@ public class MemoryFlowDispatcher implements FlowDispatcher {
     private final Map<String, List<FlowTask>> taskTypeMap = new HashMap<>();
     // taskId -> task
     private final Map<String, FlowTask> taskMap = new HashMap<>();
+    // 正在处理中的任务数据。key 为 taskId，value 为 task
+    private final Map<String, FlowTask> processingTask = new HashMap<>();
 
 
     @Override
@@ -38,7 +41,6 @@ public class MemoryFlowDispatcher implements FlowDispatcher {
         flowMap.put(flow.getFlowId(), flow);
 
         flow.getTasks().forEach(task -> {
-
             // 加入到 id 映射
             taskMap.put(task.getId(), task);
 
@@ -60,7 +62,11 @@ public class MemoryFlowDispatcher implements FlowDispatcher {
     public List<FlowTask> getTasks(@NonNull String flowType, int size) {
         List<FlowTask> tasks = taskTypeMap.get(flowType);
         if (!CollUtil.isEmpty(tasks)) {
-            return tasks.subList(0, Math.min(size, tasks.size()));
+            List<FlowTask> flowTasks = tasks.subList(0, Math.min(size, tasks.size()));
+            // remove from taskTypeMap
+            tasks.removeAll(flowTasks);
+            flowTasks.forEach(task -> processingTask.put(task.getId(), task));
+            return flowTasks;
         } else {
             return Collections.emptyList();
         }
@@ -70,7 +76,9 @@ public class MemoryFlowDispatcher implements FlowDispatcher {
     public FlowTask getTask(String flowType) {
         List<FlowTask> tasks = taskTypeMap.get(flowType);
         if (!CollUtil.isEmpty(tasks)) {
-            return tasks.get(0);
+            FlowTask flowTask = tasks.get(0);
+            processingTask.put(flowTask.getId(), flowTask);
+            return flowTask;
         }
         return null;
     }
@@ -78,6 +86,16 @@ public class MemoryFlowDispatcher implements FlowDispatcher {
     @Override
     public void reportTaskResult(TaskResult taskResult) {
         String taskId = taskResult.getTaskId();
+        FlowTask flowTask = processingTask.get(taskId);
+        if (flowTask == null) {
+            log.warn("task {} is not in processing", taskId);
+            return;
+        }
+        // 执行失败的任务重抛回任务队列
+        if (taskResult.getStatus() == TaskStatusEnum.FAIL) {
+//            taskTypeMap.put(flowTask.getTaskType(), flowTask);
+        }
+
 
     }
 
---
+++ core/src/main/java/com/wentong/jimu/flow/task/FlowFinalTask.java
@@ -0,0 +1,10 @@
+package com.wentong.jimu.flow.task;
+
+/**
+ * flow final task
+ */
+public interface FlowFinalTask extends FlowTask {
+
+    void onSuccess();
+
+}
